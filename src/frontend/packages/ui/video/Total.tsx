5c50d68ccffe269d1d886c6c16cbc568d16f83c0
thwrk <thwrk@proton.me>
2023-04-25 14:36:25 +0200

    :recycle: (frontend): calculate since and until from Context in Total
    If the user filter videos over the last 7 days, only three video where made in the last 7 days, the components would show the number of view in total "over 3 days" ( total of days where videos where made ) now it will show "over 7 days" ( the number of days of the selected periode)

--- src/frontend/packages/ui/video/Total.tsx
+++ src/frontend/packages/ui/video/Total.tsx
@@ -24,16 +24,16 @@ type Total = {
   videoIds: Array<string>;
 };
 
-type videoViewStoreType = {
-    [key: string] :  Array<DailyViewsResponseItem>
+
+type VideoViewStore = {
+    [key: string] :  VideoViewsResponse
 }
+
 
 export const Total = ({ videoIds }: Total) => {
 
 
-  const [videoViewStore, setVideoViewStore] = useState<videoViewStoreType>({}) 
-
-  const [totalVideoViews, setTotalVideoViews] = useState(0)
+  const [videoViewStore, setVideoViewStore] = useState<VideoViewStore>({}) 
 
   const {since, until} = useContext(DateContext)
 
@@ -52,21 +52,14 @@ export const Total = ({ videoIds }: Total) => {
           queryKey: [`videoViews-${videoId}`, since, until],
           queryFn: () => getVideoViews(videoId, since, until),
           onSuccess: (data: VideoViewsResponse) => {
-            videoViewStore[videoId] = data.daily_views
+            videoViewStore[videoId] = data
             return data;
           },
         };
       }),
   });
 
-  function calculateTotalViewOnDateRange(videoViewStore: videoViewStoreType, videoIds: Array<string>, since: number, until: number) {
-    const filtered_videos = Object.entries(videoViewStore).filter( ([vid]) => videoIds.indexOf(vid) >= 0)
-    const totalViewOfOneVideo = (daily_views: Array<DailyViewsResponseItem>) => daily_views.reduce( (acc: number, d: DailyViewsResponseItem) => acc + d.views, 0)
-    const total_views_of_all_videos = filtered_videos.reduce( 
-                                        (acc:number, [vid, daily_views]) =>  acc + totalViewOfOneVideo(daily_views) 
-                                          , 0) 
-    return total_views_of_all_videos
-  }
+
 
   function daysBetween({since, until}: DateRange){
     const millisecondsPerDay = 1000 * 60 * 60 * 24;
@@ -75,27 +68,12 @@ export const Total = ({ videoIds }: Total) => {
     return Math.round(differenceInDays);
   }
 
-  function getDateRange(videoViewStore: videoViewStoreType){
-    const videos = Object.entries(videoViewStore)
-    if (videos.length > 1){
-      const firstVideoDailyViews = videos[0][1]
-      const since = firstVideoDailyViews.length > 0 ? 
-                      new Date(firstVideoDailyViews[0].day).getTime() 
-                      : 0
-      const until = firstVideoDailyViews.length > 0 ? 
-                      new Date(firstVideoDailyViews[firstVideoDailyViews.length -1].day).getTime() 
-                      : 0
-      return {since, until}
-    } else {
-      return {since: 0, until: 0}
-    }
+  function calculateTotal(videoViews: VideoViewStore){
+    return Object.entries(videoViews).reduce(
+      (acc: number, [vid, views]) => acc + views.total ,
+      0
+      )
   }
-  
-
-  // FIXME: race condition ( see ViewsAreaGraph )
-  useEffect( () => {
-    setTotalVideoViews(0)
-  }, [videoIds, since, until])
 
   if (results.some((result) => result.isLoading))
     return <span>Loading...</span>;
@@ -103,9 +81,9 @@ export const Total = ({ videoIds }: Total) => {
   return (
     <>
       <h1>Total Views</h1>
-      <h2>{calculateTotalViewOnDateRange(videoViewStore, videoIds, since, until)} Views</h2>
+      <h2>{calculateTotal(videoViewStore)} Views</h2>
       <h2>for {videoIds.length} Videos</h2>
-      <h2>over {daysBetween(getDateRange(videoViewStore))} days</h2>
+      <h2>over {daysBetween({since, until})} days</h2>
       
     </>
   );
