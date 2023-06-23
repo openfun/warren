49e4cc05a9d839c9a51b79fb8e8f63a2bf82ef1b
Cyril Lay <cyril31180@hotmail.fr>
2023-06-23 14:31:48 +0200

    ♻️(backend) work on video views indicator
    Code cleaning and refactoring

---
+++ src/backend/core/warren/base_indicator.py
@@ -0,0 +1,27 @@
+"""Abstract class defining the interface for an indicator."""
+from abc import ABC, abstractmethod
+from typing import List
+
+from ralph.backends.http.lrs import BaseHTTP, LRSQuery
+
+
+class BaseIndicator(ABC):
+    """Base class for an indicator.
+
+    Defines the signature of the methods that should be implemented
+    for indicators. Indicators should implement this interface.
+    """
+
+    client: BaseHTTP
+
+    @abstractmethod
+    def get_lrs_query(self) -> LRSQuery:
+        """Gets the LRS query for fetching statements."""
+
+    @abstractmethod
+    def fetch_statements(self) -> List:
+        """Executes the LRS query to obtain statements required for the indicator."""
+
+    @abstractmethod
+    def compute(self):
+        """Executes the query, and perform operations to get the indicator value."""
