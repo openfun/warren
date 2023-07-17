fce90cdc0329fff4e2bebff6d957992a22464c88
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-07-17 21:35:32 +0200

    ✅(lti) add a first test
    Add a simple test on the development view to make
    the CI passes. Without any test added, test-app job
    was resulting in failure.

--- src/app/apps/development/tests.py
+++ src/app/apps/development/tests.py
@@ -1,1 +1,13 @@
 """Tests for the development app."""
+
+from django.test import TestCase, override_settings
+
+
+@override_settings(DEBUG=True)
+class DevelopmentLTIViewTest(TestCase):
+    """Test the development view."""
+
+    def test_development_route(self):
+        """The development app has a route that answers."""
+        response = self.client.get("/development/")
+        self.assertEqual(response.status_code, 200)
