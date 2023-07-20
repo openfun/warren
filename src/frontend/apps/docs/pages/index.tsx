80628952742ff469a3acbde0f9f590b392c073a1
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-07-20 22:29:48 +0200

    🏗️(frontend) create a reusable App context provider
    Introduce a new component responsible to inject all
    the required providers to make warren web app works.

--- src/frontend/apps/docs/pages/index.tsx
+++ src/frontend/apps/docs/pages/index.tsx
@@ -1,12 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DailyViews } from "ui";
+import AppProvider from "ui/provider/app";
 
-const queryClient = new QueryClient();
 export default function Docs() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <AppProvider>
       <h1>Docs</h1>
       <DailyViews videoIds={["uuid://8d386f48-3baa-4acf-8a46-0f2be4ae243e"]} />
-    </QueryClientProvider>
+    </AppProvider>
   );
 }
