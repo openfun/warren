67d496cf68d441c517fb7cd893b7f585109912a7
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-07-26 18:15:03 +0200

    ♻️(api) factorize video indicators
    Offer a reusable base video indicator class,
    to reduce code duplication between the three
    existing ones. The new base class abstracts views
    and downloads events in a general event related
    class. A new Mixin class was introduced to encapsulate
    the statements' preprocessing logic.

--- src/api/plugins/video/warren_video/indicators.py
+++ src/api/plugins/video/warren_video/indicators.py
@@ -1,6 +1,5 @@
 """"Warren video indicators."""
 
-from typing import List
 
 from ralph.backends.http import BaseHTTP
 from ralph.backends.http.async_lrs import LRSQuery
@@ -8,17 +7,18 @@ from ralph.models.xapi.concepts.constants.video import RESULT_EXTENSION_TIME
 from ralph.models.xapi.concepts.verbs.scorm_profile import CompletedVerb
 from ralph.models.xapi.concepts.verbs.tincan_vocabulary import DownloadedVerb
 from ralph.models.xapi.concepts.verbs.video import PlayedVerb
-from warren.base_indicator import BaseIndicator, pre_process_statements
+from warren.base_indicator import BaseIndicator, PreprocessMixin
 from warren.filters import DatetimeRange
-from warren.models import XAPI_STATEMENT, DailyCount, DailyCounts
+from warren.models import DailyCount, DailyCounts
 from warren_video.conf import settings as video_plugin_settings
 
 
-class DailyVideoViews(BaseIndicator):
-    """Daily Video Views indicator.
+class BaseDailyEvent(BaseIndicator, PreprocessMixin):
+    """Base Daily Event indicator.
 
-    Defines the daily video views indicator. It is, for a given video, and
-    a date range, the total number of views, and the number of views per day.
+    This class defines a base daily event indicator. Given an event type, a video,
+    and a date range, it calculates the total number of events and the number
+    of events per day.
     """
 
     def __init__(
@@ -26,242 +26,135 @@ class DailyVideoViews(BaseIndicator):
         client: BaseHTTP,
         video_id: str,
         date_range: DatetimeRange,
-        is_unique: bool,
+        remove_duplicate_actors: bool,
     ):
-        """Instantiate the indicator with its parameters.
+        """Instantiate the Daily Event Indicator.
 
         Args:
-            client: The LRS backend to query
+            client (BaseHTTP): The LRS backend to query.
             video_id: The ID of the video on which to compute the metric
             date_range: The date range on which to compute the indicator. It has
                 2 fields, `since` and `until` which are dates or timestamps that must be
                 in ISO format (YYYY-MM-DD, YYYY-MM-DDThh:mm:ss.sss±hh:mm or
                 YYYY-MM-DDThh:mm:ss.sssZ")
-            is_unique: If true, multiple views by the same actor are counted as
-                one
+            remove_duplicate_actors (bool): If True, filter out rows with duplicate
+                'actor.uid'.
         """
         self.client = client
+        self.remove_duplicate_actors = remove_duplicate_actors
         self.video_id = video_id
         self.date_range = date_range
-        self.is_unique = is_unique
 
-    def get_lrs_query(self) -> LRSQuery:
-        """Returns the LRS query for fetching required statements."""
+    def get_lrs_query(
+        self,
+    ) -> LRSQuery:
+        """Get the LRS query for fetching required statements."""
         return LRSQuery(
             query={
-                "verb": PlayedVerb().id,
+                "verb": self.verb_id,
                 "activity": self.video_id,
                 "since": self.date_range.since.isoformat(),
                 "until": self.date_range.until.isoformat(),
             }
         )
 
-    async def fetch_statements(self) -> List[XAPI_STATEMENT]:
-        """Executes the LRS query to obtain statements required for this indicator."""
-        return [
+    async def fetch_statements(self) -> None:
+        """Execute the LRS query to obtain statements required for this indicator."""
+        self.raw_statements = [
             value
             async for value in self.client.read(
                 target=self.client.statements_endpoint, query=self.get_lrs_query()
             )
         ]
 
-    async def compute(self) -> DailyCounts:
-        """Fetches statements and computes the current indicator.
+    def filter_statements(self) -> None:
+        """Filter statements required for this indicator.
 
-        Fetches the statements from the LRS, filters and aggregates them to return the
-        number of video views per day.
+        If necessary, this method removes any duplicate actors from the statements.
+        This filtering step is typically done to ensure that each actor's
+        contributions are counted only once.
+        """
+        if self.remove_duplicate_actors:
+            self.statements.drop_duplicates(subset="actor.uid", inplace=True)
+
+    async def compute(self) -> DailyCounts:
+        """Fetch statements and computes the current indicator.
+
+        Fetch the statements from the LRS, filter and aggregate them to return the
+        number of video events per day.
         """
         # Initialize daily counts within the specified date range,
-        # with counts initialized to zero
+        # with counts equal to zero
         daily_counts = DailyCounts.from_range(self.date_range)
-        raw_statements = await self.fetch_statements()
-        if not raw_statements:
-            return daily_counts
-        flattened = pre_process_statements(raw_statements)
+        await self.fetch_statements()
 
-        # Filter out video played events before the configured time threshold
+        if not self.raw_statements:
+            return daily_counts
+
+        self.preprocess_statements()
+        self.filter_statements()
+
+        # Compute daily counts from 'statements' DataFrame
+        # and merge them into the 'daily_counts' object
+        daily_counts.merge_counts(
+            [
+                DailyCount(date=date, count=count)
+                for date, count in self.statements.groupby("date").size().items()
+            ]
+        )
+        return daily_counts
+
+
+class DailyViews(BaseDailyEvent):
+    """Daily Completed Views indicator.
+
+    Calculate the total and daily counts of views.
+
+    Inherit from BaseDailyEvent, which provides functionality for calculating
+    indicators based on different xAPI verbs.
+    """
+
+    verb_id = PlayedVerb().id
+
+    def filter_statements(self) -> None:
+        """Filter view statements based on additional conditions.
+
+        This method filters the view statements inherited from the base indicator.
+        In addition to the base filtering, view statements are further filtered
+        based on their duration to match a minimum viewing threshold.
+        """
+        super().filter_statements()
+
         def filter_view_duration(row):
             return (
                 row[f"result.extensions.{RESULT_EXTENSION_TIME}"]
                 >= video_plugin_settings.VIEWS_COUNT_TIME_THRESHOLD
             )
 
-        # Apply the filtering function to each row
-        filtered_view_duration = flattened[
-            flattened.apply(filter_view_duration, axis=1)
+        self.statements = self.statements[
+            self.statements.apply(filter_view_duration, axis=1)
         ]
 
-        # Filter out duplicate 'actor.uid' if 'is_unique' is selected.
-        if self.is_unique:
-            filtered_view_duration.drop_duplicates(subset="actor.uid", inplace=True)
 
-        # Compute daily counts from 'filtered_view_duration' DataFrame
-        # and merge them into the 'indicator' DailyCounts object
-        daily_counts.merge_counts(
-            [
-                DailyCount(date=date, count=count)
-                for date, count in filtered_view_duration.groupby("date").size().items()
-            ]
-        )
-        return daily_counts
+class DailyCompletedViews(BaseDailyEvent):
+    """Daily Completed Views indicator.
 
+    Calculate the total and daily counts of completed views.
 
-class DailyCompletedVideoViews(BaseIndicator):
-    """Daily Completed Video Views indicator.
-
-    For a given video, and a date range, this indicator computes the total number
-    of `completed` views events, and the number of views per day.
+    Inherit from BaseDailyEvent, which provides functionality for calculating
+    indicators based on different xAPI verbs.
     """
 
-    def __init__(
-        self,
-        client: BaseHTTP,
-        video_id: str,
-        date_range: DatetimeRange,
-        is_unique: bool,
-    ):
-        """Instantiate the indicator with its parameters.
-
-        Args:
-            client: The LRS backend from which the query is to be issued
-            video_id: The ID of the video on which to compute the metric
-            date_range: The date range on which to compute the indicator. It has
-                2 fields, `since` and `until` which are dates or timestamps that must be
-                in ISO format (YYYY-MM-DD or YYYY-MM-DDTHH:mm:ss.SSSZ")
-            is_unique: If true, multiple views by the same actor are counted as
-                one
-        """
-        self.client = client
-        self.video_id = video_id
-        self.date_range = date_range
-        self.is_unique = is_unique
-
-    def get_lrs_query(self) -> LRSQuery:
-        """Returns the LRS query for fetching required statements."""
-        return LRSQuery(
-            query={
-                "verb": CompletedVerb().id,
-                "activity": self.video_id,
-                "since": self.date_range.since.isoformat(),
-                "until": self.date_range.until.isoformat(),
-            }
-        )
-
-    async def fetch_statements(self) -> List[XAPI_STATEMENT]:
-        """Executes the LRS query to obtain statements required for this indicator."""
-        return [
-            value
-            async for value in self.client.read(
-                target=self.client.statements_endpoint, query=self.get_lrs_query()
-            )
-        ]
-
-    async def compute(self) -> DailyCounts:
-        """Fetches statements and computes the current indicator.
-
-        Fetches the statements from the LRS, filters and aggregates them to return the
-        number of video views per day.
-        """
-        # Initialize daily counts within the specified date range,
-        # with counts initialized to zero
-        daily_counts = DailyCounts.from_range(self.date_range)
-        raw_statements = await self.fetch_statements()
-
-        if not raw_statements:
-            return daily_counts
-        flattened = pre_process_statements(raw_statements)
-
-        # Filter out duplicate 'actor.uid' if 'is_unique' is selected.
-        if self.is_unique:
-            flattened.drop_duplicates(subset="actor.uid", inplace=True)
-
-        # Compute daily counts from 'flattened' DataFrame
-        # and merge them into the 'indicator' DailyCounts object
-        daily_counts.merge_counts(
-            [
-                DailyCount(date=date, count=count)
-                for date, count in flattened.groupby("date").size().items()
-            ]
-        )
-        return daily_counts
+    verb_id = CompletedVerb().id
 
 
-class DailyVideoDownloads(BaseIndicator):
-    """Daily Video Downloads indicator.
+class DailyDownloads(BaseDailyEvent):
+    """Daily Downloads indicator.
 
-    Defines the daily video downloads indicator. It is, for a given video, and
-    a date range, the total number of downloads, and the number of downloads per day.
+    Calculate the total and daily counts of downloads.
+
+    Inherit from BaseDailyEvent, which provides functionality for calculating
+    indicators based on different xAPI verbs.
     """
 
-    def __init__(
-        self,
-        client: BaseHTTP,
-        video_id: str,
-        date_range: DatetimeRange,
-        is_unique: bool,
-    ):
-        """Instantiate the indicator with its parameters.
-
-        Args:
-            client: The LRS backend to query
-            video_id: The ID of the video on which to compute the metric
-            date_range: The date range on which to compute the indicator. It has
-                2 fields, `since` and `until` which are dates or timestamps that must be
-                in ISO format (YYYY-MM-DD, YYYY-MM-DDThh:mm:ss.sss±hh:mm or
-                YYYY-MM-DDThh:mm:ss.sssZ")
-            is_unique: If true, multiple downloads by the same actor are counted as one
-        """
-        self.client = client
-        self.video_id = video_id
-        self.date_range = date_range
-        self.is_unique = is_unique
-
-    def get_lrs_query(self) -> LRSQuery:
-        """Returns the LRS query for fetching required statements."""
-        return LRSQuery(
-            query={
-                "verb": DownloadedVerb().id,
-                "activity": self.video_id,
-                "since": self.date_range.since.isoformat(),
-                "until": self.date_range.until.isoformat(),
-            }
-        )
-
-    async def fetch_statements(self) -> List[XAPI_STATEMENT]:
-        """Executes the LRS query to obtain statements required for this indicator."""
-        return [
-            value
-            async for value in self.client.read(
-                target=self.client.statements_endpoint, query=self.get_lrs_query()
-            )
-        ]
-
-    async def compute(self) -> DailyCounts:
-        """Fetches statements and computes the current indicator.
-
-        Fetches the statements from the LRS, filters and aggregates them to return the
-        number of video downloads per day.
-        """
-        # Initialize daily counts within the specified date range,
-        # with counts initialized to zero
-        daily_counts = DailyCounts.from_range(self.date_range)
-        raw_statements = await self.fetch_statements()
-        if not raw_statements:
-            return daily_counts
-        preprocessed_statements = pre_process_statements(raw_statements)
-
-        # Filter out duplicate 'actor.uid' if 'is_unique' is selected.
-        if self.is_unique:
-            preprocessed_statements.drop_duplicates(subset="actor.uid", inplace=True)
-
-        # Compute daily counts from 'preprocessed_statements' DataFrame
-        # and merge them into the 'indicator' DailyCounts object
-        daily_counts.merge_counts(
-            [
-                DailyCount(date=date, count=count)
-                for date, count in preprocessed_statements.groupby("date")
-                .size()
-                .items()
-            ]
-        )
-        return daily_counts
+    verb_id = DownloadedVerb().id
