76d62728adda5203bbf39b467ab73b55bb97e46b
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-08-16 10:55:42 +0200

    🏗️(frontend) restructure frontend for a single page application
    In the process of integrating Warren's LTI capabilities, we encountered the need
    to inject a single JS bundle into the core Django template. Given these specific
    requirements, statically bundling the demo app using the Next.js didn't align
    well. Drawing inspiration from the Cunningham monorepo project, we opted to
    bundle the frontend using vite.js.
    
    It's worth noting that Warren's configurations are inspired by Cunningham's
    setup, while Marsha's LTI rendering strategies provide a guidance. This choice
    ensures an alignment with best practices and other Fun projects.

---
+++ src/frontend/apps/web/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    hmr: {
+      port: 3001,
+    },
+  },
+});
--- src/frontend/packages/ui/libs/axios.ts
+++ src/frontend/packages/ui/libs/axios.ts
@@ -1,5 +1,5 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_WARREN_BACKEND_ROOT_URL}/api/v1/`,
+  baseURL: `${import.meta.env.VITE_PUBLIC_WARREN_BACKEND_ROOT_URL}/api/v1/`,
 });
