320ccee319b8813483f457a5d1a296b0db6fe496
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-08-17 15:55:24 +0200

    ✨(frontend) implement DOM parsing for extracting app data
    The Django server currently embeds data into the DOM during the rendering of
    the SPA. This update introduces a new parsing function designed to retrieve
    the essential application data within the React SPA. These extracted data points
    play a crucial role in user authentication, configuring the app, and presenting
    the appropriate resources.
    
    It's important to note that this implementation is not final. Future plans
    involve refactoring the code, especially with the addition of app routing and
    initialization logic.

--- src/frontend/apps/web/src/App.tsx
+++ src/frontend/apps/web/src/App.tsx
@@ -3,8 +3,13 @@ import AppProvider from "ui/provider/app";
 import Filters from "ui/components/filters";
 import { DailyViews } from "ui";
 import Layout from "../components/Layout";
+import { parseDataContext } from "./utils";
+
+const dataContext = parseDataContext();
 
 export const App = () => {
+  // todo : store this data in a proper context and initialize the app
+  console.log(dataContext); // eslint-disable-line no-console
   return (
     <Layout>
       <AppProvider>
