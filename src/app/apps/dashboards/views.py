0487be1f35a7aba0a03103da5944379d48eee6f2
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-08-17 15:53:11 +0200

    🔧(frontend) render the Web app with the Django LTI server
    Replaced the conventional development server with a process that builds the
    frontend project, and then stores its output within Django's static files.
    
    Django injects the JS bundle and the stylesheet in its base template. SPA
    wait for the DOM to be mounted, and thus render the app in a dedicated div.
    This allows Django server to write data to the DOM, thus making accessible
    data to the SPA, that parses the DOM.
    
    Additionally, the allowed host has been updated to reflect the Django server
    port. This adjustment is vital for seamless API calls from the frontend in the
    future.

--- src/app/apps/dashboards/views.py
+++ src/app/apps/dashboards/views.py
@@ -6,3 +6,11 @@ class DashboardView(TemplateView):
     """Dummy dashboard view."""
 
     template_name = "dashboards/base.html"
+
+    def _build_app_data(self):
+        return {"key": "woop."}
+
+    def get_context_data(self):
+        """Enrich view's context with app's data."""
+        parent_context = super().get_context_data()
+        return {"app_data": self._build_app_data(), **parent_context}
--- src/frontend/apps/web/vite.config.ts
+++ src/frontend/apps/web/vite.config.ts
@@ -4,11 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    hmr: {
-      port: 3001,
+  build: {
+    outDir: "../../../app/staticfiles/js/build",
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
     },
   },
 });
