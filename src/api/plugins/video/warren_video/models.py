b9c28472334760bbdfa9f8451c8876d69136e82e
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-08-30 17:50:28 +0200

    fixup! 🚧 wip

--- src/api/plugins/video/warren_video/models.py
+++ src/api/plugins/video/warren_video/models.py
@@ -3,29 +3,10 @@ from typing import Union
 
 from pydantic import BaseModel, validator
 
-Seconds = Union[int, None]
-Percent = Union[float, None]
-
 
 class Info(BaseModel):
     """Base model to represent information for a video."""
 
     name: str | None
-    length: Seconds
-    completion_threshold: Percent
-
-    @validator("length")
-    @classmethod
-    def length_must_be_positive(cls, v: Seconds) -> Seconds:
-        """Validate video's length."""
-        if v is not None and v < 0:
-            raise ValueError("must be positive.")
-        return v
-
-    @validator("completion_threshold")
-    @classmethod
-    def completion_threshold_must_be_valid(cls, v: Percent) -> Percent:
-        """Validate video's completion threshold."""
-        if v is not None and (v < 0 or v > 1):
-            raise ValueError("must be between 0 and 1.")
-        return v
+    length: Union[PositiveInt, None]  # in Seconds
+    completion_threshold: Union[confloat(gt=0.0, lt=1.0), None]  # in Percent
