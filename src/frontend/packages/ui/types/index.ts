6f67c9704e463dba836b30a539c8c727d49a9382
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-09-05 16:56:25 +0200

    🛂(frontend) implement JWT Storage and Injection for API Requests
    Receive the initial JWT from the DOM, issued by the Django LTI app.
    
    The JWT is stored in a context, and then injected in requests to
    Warren API. Frontend refreshes expired access token and retries
    the request.

--- src/frontend/packages/ui/types/index.ts
+++ src/frontend/packages/ui/types/index.ts
@@ -4,4 +4,6 @@ export interface AppData {
   };
   lti_select_form_action_url: string;
   lti_route: string;
+  access: string;
+  refresh: string;
 }
--- src/frontend/packages/ui/video/vite-end.d.ts
+++ src/frontend/packages/ui/video/vite-end.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_PUBLIC_WARREN_BACKEND_ROOT_URL: string;
+  readonly VITE_PUBLIC_WARREN_API_ROOT_URL: string;
+  readonly VITE_PUBLIC_WARREN_APP_ROOT_URL: string;
 }
 
 interface ImportMeta {
