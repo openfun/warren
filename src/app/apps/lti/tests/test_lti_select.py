53f1d081197ba53b7e696c5ac6e2f536e47175a3
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-09-08 09:38:10 +0200

    ✅(app) add tests on LTISelectView class
    Added tests to enhance code quality. These tests are
    inspired by Marsha and django-lti-toolbox repos."

---
+++ src/app/apps/lti/tests/test_lti_select.py
@@ -0,0 +1,169 @@
+"""Tests for the LTI select view."""
+
+
+import json
+import re
+from html import unescape
+from logging import Logger
+from unittest import mock
+from urllib.parse import urlencode
+
+from django.test import TestCase, override_settings
+from lti_toolbox.factories import LTIConsumerFactory, LTIPassportFactory
+from lti_toolbox.launch_params import LTIMessageType
+from lti_toolbox.utils import CONTENT_TYPE, sign_parameters
+
+TARGET_URL_PATH = "/lti/select"
+
+
+class LTISelectViewTestCase(TestCase):
+    """Test the LTI select view."""
+
+    def setUp(self):
+        """Set up an LTI consumer and passport for the tests."""
+        super().setUp()
+        self._consumer = LTIConsumerFactory(
+            slug="test_lti", title="test consumer", url="http://fake-lms.com"
+        )
+        self._passport = LTIPassportFactory(
+            title="test passport", consumer=self._consumer
+        )
+
+    @override_settings(ALLOWED_HOSTS=["fake-lms.com"])
+    @mock.patch.object(Logger, "debug")
+    def test_views_lti_select_invalid_request_type(self, mock_logger):
+        """Validate that view fails when the LTI request type is a basic launch."""
+        # Its type is not a Content-Item selection request.
+        lti_parameters = {
+            "lti_message_type": "basic-lti-launch-request",
+            "lti_version": "LTI-1p0",
+            "resource_link_id": "df7",
+            "context_id": "1",
+            "lis_person_sourcedid": "1",
+            "lis_person_contact_email_primary": "contact@example.com",
+        }
+
+        signed_parameters = sign_parameters(
+            self._passport, lti_parameters, f"http://fake-lms.com{TARGET_URL_PATH}"
+        )
+
+        response = self.client.post(
+            TARGET_URL_PATH,
+            urlencode(signed_parameters),
+            content_type=CONTENT_TYPE,
+            HTTP_REFERER="http://fake-lms.com",
+            HTTP_HOST="fake-lms.com",
+        )
+        self.assertEqual(response.status_code, 403)
+        mock_logger.assert_called_with("LTI message type is not valid.")
+
+    @override_settings(ALLOWED_HOSTS=["fake-lms.com", "wrongserver.com"])
+    @mock.patch.object(Logger, "info")
+    def test_views_lti_select_invalid_signature(self, mock_logger):
+        """Validate that view fails when the LTI signature is invalid."""
+        lti_parameters = {
+            "lti_message_type": "ContentItemSelectionRequest",
+            "lti_version": "LTI-1p0",
+            "accept_media_types": "application/vnd.ims.lti.v1.ltilink",
+            "accept_presentation_document_targets": "frame,iframe,window",
+            "content_item_return_url": "http://testserver/",
+        }
+
+        signed_parameters = sign_parameters(
+            self._passport, lti_parameters, f"http://fake-lms.com{TARGET_URL_PATH}"
+        )
+
+        # The POST request is initiated from a distinct host compared to
+        # the one that's responsible for signing its payload.
+        response = self.client.post(
+            TARGET_URL_PATH,
+            urlencode(signed_parameters),
+            content_type=CONTENT_TYPE,
+            HTTP_REFERER="https://wrongserver",
+            HTTP_HOST="wrongserver.com",
+        )
+        self.assertEqual(response.status_code, 403)
+        mock_logger.assert_called_with("Valid signature: %s", False)
+
+    @override_settings(ALLOWED_HOSTS=["fake-lms.com"])
+    @mock.patch.object(Logger, "debug")
+    def test_views_lti_select_invalid_user(self, mock_logger):
+        """Validate that view fails when the LTI user is invalid."""
+        # LTI user information are missing.
+        lti_parameters = {
+            "lti_message_type": "ContentItemSelectionRequest",
+            "lti_version": "LTI-1p0",
+            "accept_media_types": "application/vnd.ims.lti.v1.ltilink",
+            "accept_presentation_document_targets": "frame,iframe,window",
+            "content_item_return_url": "http://testserver/",
+        }
+
+        signed_parameters = sign_parameters(
+            self._passport, lti_parameters, f"http://fake-lms.com{TARGET_URL_PATH}"
+        )
+
+        response = self.client.post(
+            TARGET_URL_PATH,
+            urlencode(signed_parameters),
+            content_type=CONTENT_TYPE,
+            HTTP_REFERER="http://fake-lms.com",
+            HTTP_HOST="fake-lms.com",
+        )
+        self.assertEqual(response.status_code, 403)
+        mock_logger.assert_called_with(
+            "LTI user is not valid: %s",
+            {
+                "course": ["Ce champ est obligatoire."],
+                "user": ["Ce champ est obligatoire."],
+                "email": ["Ce champ est obligatoire."],
+            },
+        )
+
+    @override_settings(ALLOWED_HOSTS=["fake-lms.com"])
+    def test_views_lti_select_valid(self):
+        """Validate that view is correctly rendered."""
+        lti_parameters = {
+            "lti_message_type": "ContentItemSelectionRequest",
+            "lti_version": "LTI-1p0",
+            "accept_media_types": "application/vnd.ims.lti.v1.ltilink",
+            "accept_presentation_document_targets": "frame,iframe,window",
+            "content_item_return_url": "http://fake-lms.com/",
+            "context_id": "1",
+            "lis_person_sourcedid": "1",
+            "lis_person_contact_email_primary": "contact@example.com",
+        }
+
+        signed_parameters = sign_parameters(
+            self._passport, lti_parameters, f"http://fake-lms.com{TARGET_URL_PATH}"
+        )
+
+        response = self.client.post(
+            TARGET_URL_PATH,
+            urlencode(signed_parameters),
+            content_type=CONTENT_TYPE,
+            HTTP_REFERER="http://fake-lms.com",
+            HTTP_HOST="fake-lms.com",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<html>")
+
+        # Get the context data passed to the frontend from the DOM
+        match = re.search(
+            '<div id="warren-frontend-data" data-context="(.*)"></div>',
+            response.content.decode("utf-8"),
+        )
+
+        # Load its json content
+        context = json.loads(unescape(match.group(1)))
+
+        # Check that the frontend would route to the selection form
+        # and respond the correct view
+        self.assertEqual(context["lti_route"], "select")
+        self.assertEqual(context["lti_select_form_action_url"], "/lti/respond")
+
+        # LTI message type has to be changed from a
+        # SELECTION_REQUEST to a SELECTION_RESPONSE
+        signed_parameters.update(
+            {"lti_message_type": LTIMessageType.SELECTION_RESPONSE.value}
+        )
+        self.assertEqual(context["lti_select_form_data"], signed_parameters)
