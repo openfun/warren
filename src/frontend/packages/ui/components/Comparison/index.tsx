7afddaa6e3091d847dcf236a7fc432601173bf9a
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-10-12 23:27:58 +0200

    ✨(frontend) introduce a Comparison component
    Introducing the Comparison component within the existing Metric component,
    enabling the display of percentage changes in a Metric value in relation
    to the previous period of the same length.

---
+++ src/frontend/packages/ui/components/Comparison/index.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo } from "react";
+import classNames from "classnames";
+import { MetricProps } from "../Metrics";
+
+const formatPercent = (percent: number) => `${(percent * 100).toFixed(0)}%`;
+
+export interface ComparisonProps
+  extends Pick<MetricProps, ["metric", "comparison"]> {}
+
+/**
+ * A React component for displaying a comparison metric with a dynamic icon and percentage change.
+ *
+ * This component takes two metrics, 'metric' and 'comparison', and calculates the percentage change between them.
+ * It displays an icon and percentage value, with optional fetching state indicators and tooltips.
+ *
+ * @param {ComparisonProps} props - The component's props.
+ * @param {ValueWithFetchState} props.metric - The primary metric data, including a numeric value and an optional fetching state.
+ * @param {ValueWithFetchState} props.comparison - The comparison metric data, including a numeric value and an optional fetching state.
+ * @returns {JSX.Element} The JSX for the Comparison component.
+ */
+export const Comparison: React.FC<ComparisonProps> = ({
+  metric,
+  comparison,
+}) => {
+  const comparisonToNull = useMemo(() => comparison.value === 0, [comparison]);
+
+  const percent = useMemo(
+    () =>
+      !comparisonToNull && (metric.value - comparison.value) / comparison.value,
+    [metric, comparison],
+  );
+
+  const isStrictlyNegative = useMemo(() => percent < 0, [percent]);
+  const isStrictlyPositive = useMemo(() => percent > 0, [percent]);
+
+  const iconName = useMemo(() => {
+    if (percent === 0) {
+      return "horizontal_rule";
+    }
+    return `arrow_${isStrictlyNegative ? "down" : "up"}ward`;
+  }, [percent, isStrictlyNegative]);
+
+  return (
+    <div
+      className={classNames(["c__comparison"], {
+        "c__comparison--fetching": comparison.isFetching || metric.isFetching,
+        "c__comparison--negative": isStrictlyNegative,
+        "c__comparison--positive": isStrictlyPositive,
+      })}
+    >
+      {comparison.isFetching || metric.isFetching || comparisonToNull ? (
+        <span className="material-icons">horizontal_rule</span>
+      ) : (
+        <>
+          <span className="material-icons">{iconName}</span>
+          <p>{percent ? formatPercent(percent) : ""}</p>
+        </>
+      )}
+    </div>
+  );
+};
--- src/frontend/packages/ui/video/components/Metrics/ViewsMetric.tsx
+++ src/frontend/packages/ui/video/components/Metrics/ViewsMetric.tsx
@@ -1,8 +1,26 @@
 import React, { useMemo } from "react";
+import dayjs from "dayjs";
 import useFilters from "../../hooks/useFilters";
 import { useVideosViews } from "../../api/getVideoViews";
-import { sumViews } from "../../utils";
+import { formatDates, sumViews } from "../../utils";
 import { Metric, MetricProps } from "../../../components/Metrics";
+
+/**
+ * Calculate the previous time period based on the provided since and until dates.
+ *
+ * This function calculates the start and end dates of the previous time period relative to the given 'since' and 'until' dates.
+ * It computes the duration of the selected period and calculates the previous period's start and end dates accordingly.
+ *
+ * @param {string} since - The start date of the current period.
+ * @param {string} until - The end date of the current period.
+ * @returns {string[]} An array of two ISO date strings representing the start and end dates of the previous time period.
+ */
+const previousPeriod = (since: string, until: string) => {
+  const periodDuration = dayjs(until).diff(since, "day") + 1;
+  return formatDates(
+    [periodDuration, 1].map((offset) => dayjs(since).subtract(offset, "day")),
+  );
+};
 
 export interface ViewsMetricProps extends MetricProps {
   unique?: boolean;
@@ -14,6 +32,7 @@ export interface ViewsMetricProps extends MetricProps {
  *
  * A React functional component that displays a metric related to selected videos' views.
  * The metric can be parametrized based on various options such as uniqueness and completeness.
+ * It also includes a comparison with the previous period of the same length.
  *
  * @component
  * @param {ViewsMetricProps} props - The props for the ViewsMetric component.
@@ -34,20 +53,37 @@ export const ViewsMetric: React.FC<ViewsMetricProps> = ({
     videoIds,
   } = useFilters();
 
+  const params = { unique, complete };
+
   const { videoViews, isFetching } = useVideosViews(videoIds, {
     since,
     until,
-    unique,
-    complete,
+    ...params,
   });
   const total = useMemo(() => sumViews(videoViews), [videoViews]);
+
+  const [comparisonSince, comparisonUntil] = useMemo(
+    () => previousPeriod(since, until),
+    [since, until],
+  );
+  const { videoViews: comparisonData, isFetching: isComparisonFetching } =
+    useVideosViews(videoIds, {
+      since: comparisonSince,
+      until: comparisonUntil,
+      ...params,
+    });
+
+  const comparison = useMemo(() => sumViews(comparisonData), [comparisonData]);
 
   return (
     <Metric
       title={title}
       tooltip={tooltip}
-      value={total}
-      isFetching={isFetching}
+      metric={{ value: total, isFetching }}
+      comparison={{
+        value: comparison,
+        isFetching: isComparisonFetching,
+      }}
     />
   );
 };
