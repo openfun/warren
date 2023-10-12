366c5bed7fe65cac461d7397f0ad952908e19632
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-10-12 23:27:58 +0200

    ✨(frontend) introduce a Comparison component
    Introducing the Comparison component within the existing Metric component,
    enabling the display of percentage changes in a Metric value in relation
    to the previous period of the same length.

--- src/frontend/packages/ui/components/Metrics/index.tsx
+++ src/frontend/packages/ui/components/Metrics/index.tsx
@@ -1,31 +1,39 @@
 import React from "react";
 import classNames from "classnames";
 import { Card, CardProps } from "../Card";
+import { Comparison } from "../Comparison";
 
-export interface MetricProps extends Omit<CardProps, "children"> {
+export type ValueWithFetchState = {
   value: number;
   isFetching?: boolean;
+};
+
+export interface MetricProps extends Omit<CardProps, "children"> {
+  metric: ValueWithFetchState;
+  comparison?: ValueWithFetchState;
 }
 
 /**
- * A reusable component for displaying a metric with a title, value, and optional tooltip.
+ * A reusable component for displaying a metric with a title, value, optional tooltip,
+ * and an optional comparison metric.
  *
  * @param {MetricProps} props - The component's props.
+ * @param {string} props.title - The title of the metric.
+ * @param {string} props.tooltip - An optional tooltip for the metric.
+ * @param {ValueWithFetchState} props.metric - The main metric data, including a numeric value and an optional fetching state.
+ * @param {ValueWithFetchState} props.comparison - An optional comparison metric data, also including a numeric value and an optional fetching state.
  * @returns {JSX.Element} The JSX for the Metric component.
  */
-export const Metric: React.FC<MetricProps> = ({
-  title,
-  tooltip,
-  value,
-  isFetching,
-}) => (
-  <Card title={title} tooltip={tooltip}>
+// TODO - refactor the component's API.
+export const Metric: React.FC<MetricProps> = (props) => (
+  <Card title={props.title} tooltip={props.tooltip}>
     <h4
       className={classNames(["c__metric__value"], {
-        "c__metric__value--fetching": isFetching,
+        "c__metric__value--fetching": props.metric.isFetching,
       })}
     >
-      {value}
+      {props.metric.value}
     </h4>
+    {props.comparison !== undefined && <Comparison {...props} />}
   </Card>
 );
