4405d2e204007e2ce73c7df49dc2fb5204159deb
Julien Maupetit <julien@maupetit.net>
2023-10-17 11:34:16 +0200

    ♻️(api) make indicators immutable
    There should be one obvious way for an indicator to be computed. One
    indicator instance should be used per calculation, we've changed methods
    signature with this idea in mind. Event if its not the case per se,
    indicators should be considered as "immutable" objects.

--- src/api/core/warren/tests/test_indicators_base.py
+++ src/api/core/warren/tests/test_indicators_base.py
@@ -39,39 +39,6 @@ async def test_base_indicator_fetch_statements_with_default_query(
 
 
 @pytest.mark.anyio
-async def test_base_indicator_fetch_statements_with_custom_query(
-    httpx_mock: HTTPXMock,
-):
-    """Test the `fetch_statements` method of the base indicator using a custom
-    LRS query.
-
-    """  # noqa: D205
-
-    class MyIndicator(BaseIndicator):
-        def get_lrs_query(self):
-            return LRSQuery(query={"verb": "played"})
-
-        async def compute(self):
-            return None
-
-    my_indicator = MyIndicator()
-    my_indicator.lrs_client.base_url = "http://fake-lrs.com"
-
-    # Mock the LRS call so that it returns the fixture statements
-    httpx_mock.add_response(
-        url="http://fake-lrs.com/xAPI/statements?verb=downloaded&limit=0",
-        method="GET",
-        json={"statements": [{"id": 1}, {"id": 2}, {"id": 3}]},
-        status_code=200,
-    )
-
-    statements = await my_indicator.fetch_statements(
-        lrs_query=LRSQuery(query={"verb": "downloaded", "limit": 0})
-    )
-    assert len(statements) == 3
-
-
-@pytest.mark.anyio
 async def test_base_indicator_fetch_statements_with_lrs_failure(
     httpx_mock: HTTPXMock,
 ):
@@ -99,3 +66,80 @@ async def test_base_indicator_fetch_statements_with_lrs_failure(
 
     with pytest.raises(LrsClientException):
         await my_indicator.fetch_statements()
+
+
+def test_base_indicator_replace():
+    """Test the base indicator _replace method."""
+
+    class MyIndicator(BaseIndicator):
+        def __init__(self, span_range=None, foo=None, bar=None):
+            super().__init__(span_range=span_range, foo=foo, bar=bar)
+
+        def get_lrs_query(self):
+            return None
+
+        async def compute(self):
+            return None
+
+    indicator = MyIndicator(foo="lol", bar=[1, 2, 3])
+    assert indicator.span_range is None
+    assert indicator.foo == "lol"
+    assert indicator.bar == [1, 2, 3]
+
+    # Shallow copy
+    copied = indicator._replace(foo="baz", bar=[4, 5, 6])
+    assert copied.span_range is None
+    assert copied.foo == "baz"
+    assert copied.bar == [4, 5, 6]
+
+    # Deep copy
+    copied = indicator._replace(deep=True, foo="baz", bar=[4, 5, 6])
+    assert copied.span_range is None
+    assert copied.foo == "baz"
+    assert copied.bar == [4, 5, 6]
+
+    # Proceed by reference to check differences between shallow and deep copies
+    bar = [1, 2, 3]
+    indicator = MyIndicator(foo="lol", bar=bar)
+
+    shallow = indicator._replace(deep=False, foo="baz")
+    deep = indicator._replace(deep=True, foo="baz")
+    assert shallow.bar == [1, 2, 3]
+    assert deep.bar == [1, 2, 3]
+
+    bar.append(4)
+    assert indicator.bar == shallow.bar == [1, 2, 3, 4]
+    assert deep.bar == [1, 2, 3]
+
+
+def test_base_indicator_child_immutability():
+    """Check indicators immutability."""
+
+    class MyIndicator(BaseIndicator):
+        def __init__(self, span_range=None, foo=None, bar=None):
+            super().__init__(span_range=span_range, foo=foo, bar=bar)
+
+        def get_lrs_query(self):
+            return None
+
+        async def compute(self):
+            return None
+
+    indicator = MyIndicator(foo="lol", bar=[1, 2, 3])
+
+    assert indicator.span_range is None
+    assert indicator.foo == "lol"
+    assert indicator.bar == [1, 2, 3]
+
+    with pytest.raises(AttributeError, match="Can't set attribute 'span_range'"):
+        indicator.span_range = [1, 2]
+    with pytest.raises(AttributeError, match="Can't set attribute 'foo'"):
+        indicator.foo = "spam"
+    with pytest.raises(AttributeError, match="Can't set attribute 'bar'"):
+        indicator.bar = 4
+    with pytest.raises(AttributeError, match="Can't delete attribute 'span_range'"):
+        del indicator.span_range
+    with pytest.raises(AttributeError, match="Can't delete attribute 'foo'"):
+        del indicator.foo
+    with pytest.raises(AttributeError, match="Can't delete attribute 'bar'"):
+        del indicator.bar
