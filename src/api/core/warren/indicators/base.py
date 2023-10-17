10d0bff597e658f54b8ec30c512f4021b21adac2
Julien Maupetit <julien@maupetit.net>
2023-10-17 11:34:16 +0200

    ♻️(api) make indicators immutable
    There should be one obvious way for an indicator to be computed. One
    indicator instance should be used per calculation, we've changed methods
    signature with this idea in mind. Event if its not the case per se,
    indicators should be considered as "immutable" objects.

--- src/api/core/warren/indicators/base.py
+++ src/api/core/warren/indicators/base.py
@@ -1,4 +1,6 @@
 """Abstract class defining the interface for an indicator."""
+import copy
+import inspect
 from abc import ABC, abstractmethod
 from datetime import datetime
 from functools import cached_property
@@ -19,23 +21,60 @@ class BaseIndicator(ABC):
     for indicators.
     """
 
-    def __init__(self, span_range: DatetimeRange = None):
+    def __init__(self, span_range: DatetimeRange = None, **kwargs):
         """Instantiate the base indicator.
 
         Args:
             span_range (DatetimeRange): date/time span range on which
             the indicator needs to be computed
+
+            kwargs (dict): indicator-specific extra arguments that will
+            be stored as indicator attributes.
         """
-        self.span_range = span_range
+        super().__setattr__("span_range", span_range)
+        for attr, value in kwargs.items():
+            super().__setattr__(attr, value)
+
+    def __setattr__(self, name, value):
+        """Indicators are immutable, setattr is forbidden."""
+        raise AttributeError(f"Can't set attribute '{name}'")
+
+    def __delattr__(self, name):
+        """Indicators are immutable, delattr is forbidden."""
+        raise AttributeError(f"Can't delete attribute '{name}'")
+
+    def _replace(self, deep=False, **kwargs):
+        """Return an indicator copy with overridden kwargs.
+
+        Args:
+            deep (bool): if True perform a deep copy (defaults to shallow)
+            kwargs (dict): additional arguments
+
+        Disclaimer:
+            As by default we perform a shallow copy of indicator attributes,
+            you must be aware of consequencies when an indicator attribute is
+            passed as reference: the original object passed as a reference may
+            be modified in the original indicator instance AND its copy.
+        """
+        copy_ = copy.deepcopy if deep else copy.copy
+        self_args = copy_(self.__dict__)
+        other_args = {
+            param.name: self_args[param.name]
+            for param in inspect.signature(self.__init__).parameters.values()
+            if param.name in self_args
+        }
+        other_args.update(kwargs)
+
+        return self.__class__(**other_args)
 
     @property
     def since(self) -> datetime:
-        """Shorcut to the indicator date/time span minimal value."""
+        """Shortcut to the indicator date/time span minimal value."""
         return self.span_range.since
 
     @property
     def until(self) -> datetime:
-        """Shorcut to the indicator date/time span maximal value."""
+        """Shortcut to the indicator date/time span maximal value."""
         return self.span_range.until
 
     @cached_property
@@ -44,14 +83,10 @@ class BaseIndicator(ABC):
         return async_lrs_client
 
     @abstractmethod
-    def get_lrs_query(
-        self, since: datetime = None, until: datetime = None
-    ) -> LRSStatementsQuery:
+    def get_lrs_query(self) -> LRSStatementsQuery:
         """Get the LRS query for fetching statements."""
 
-    async def fetch_statements(
-        self, lrs_query: LRSStatementsQuery = None
-    ) -> List[XAPI_STATEMENT]:
+    async def fetch_statements(self) -> List[XAPI_STATEMENT]:
         """Execute the LRS query to obtain statements required for this indicator.
 
         If lrs_query is None, it defaults to self.get_lrs_query()
@@ -61,12 +96,12 @@ class BaseIndicator(ABC):
                 value
                 async for value in self.lrs_client.read(
                     target=self.lrs_client.statements_endpoint,
-                    query=lrs_query or self.get_lrs_query(),
+                    query=self.get_lrs_query(),
                 )
             ]
         except BackendException as exception:
             raise LrsClientException("Failed to fetch statements") from exception
 
     @abstractmethod
-    async def compute(self, since: datetime = None, until: datetime = None):
+    async def compute(self):
         """Execute the LRS query, and perform operations to get the indicator value."""
