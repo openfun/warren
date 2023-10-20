dcaca5f8e2cebda1c51f97bc3c02cce1b1c9dd1d
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-10-20 17:56:34 +0200

    ✨(app) send course information to the frontend
    Added the course and context info in the app data,
    passed from the Django view to the frontend.
    
    It will help us building a more informative dashboard,
    and navigating between resources.

--- src/frontend/packages/ui/types/index.ts
+++ src/frontend/packages/ui/types/index.ts
@@ -1,7 +1,15 @@
+export interface CourseData {
+  organization: string;
+  course_name: string;
+  course_run?: string;
+}
+
 export interface AppData {
   lti_select_form_jwt: string;
   lti_select_form_action_url: string;
   lti_route: string;
   access: string;
   refresh: string;
+  context_title?: string;
+  course_info?: CourseData;
 }
