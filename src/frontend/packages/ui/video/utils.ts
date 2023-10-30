8eed68281ec7c991a3bf5f1a0e572e863845fbe5
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-10-30 12:36:16 +0100

    🩹(frontend) handle DST and STD when fetching the API
    Temporary fix to align frontend calls with new API requirement:
    Start and end dates in the same timezone.
    Ongoing improvements needed.

--- src/frontend/packages/ui/video/utils.ts
+++ src/frontend/packages/ui/video/utils.ts
@@ -1,5 +1,8 @@
 import dayjs, { Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { VideoViewsResponse } from "./types";
+
+dayjs.extend(utc);
 
 /**
  * Calculate the total sum of views from an array of VideoViewsResponse objects.
@@ -14,21 +17,6 @@ export const sumViews = (views: VideoViewsResponse[]): number =>
     : 0;
 
 /**
- * Get default start and end dates for a date range.
- * The default range duration is 'DEFAULT_RANGE_DURATION' days.
- *
- * @returns {[string, string]} An array containing the default start and end dates.
- */
-export const getDefaultDates = (): [string, string] => {
-  const DEFAULT_RANGE_DURATION = 7;
-  const endDate = dayjs().endOf("day");
-  const startDate = endDate
-    .subtract(DEFAULT_RANGE_DURATION, "day")
-    .startOf("day");
-  return [startDate, endDate].map((v) => v.format());
-};
-
-/**
  * Format a pair of date values to the start or end of the day.
  *
  * This function takes an array of two date values and formats each date to either the start or end of the day based on its position in the array.
@@ -38,6 +26,27 @@ export const getDefaultDates = (): [string, string] => {
  */
 export const formatDates = (dates: [string | Dayjs, string | Dayjs]) => {
   const formatDay = (d: string | Dayjs, isStart: boolean) =>
-    dayjs(d)[isStart ? "startOf" : "endOf"]("day").format();
-  return dates.map((d, index) => formatDay(d, index === 0));
+    dayjs(d)[isStart ? "startOf" : "endOf"]("day");
+
+  const [startDate, endDate] = dates.map((d, index) =>
+    formatDay(d, index === 0),
+  );
+
+  // FIXME - Handle daylight saving time (DST) and standard time (STD) disparities within the API.
+  return [startDate.utcOffset(endDate.utcOffset(), true), endDate].map((v) =>
+    v.format(),
+  );
+};
+
+/**
+ * Get default start and end dates for a date range.
+ * The default range duration is 'DEFAULT_RANGE_DURATION' days.
+ *
+ * @returns {[string, string]} An array containing the default start and end dates.
+ */
+export const getDefaultDates = (): [string, string] => {
+  const DEFAULT_RANGE_DURATION = 7;
+  const endDate = dayjs();
+  const startDate = endDate.subtract(DEFAULT_RANGE_DURATION, "day");
+  return formatDates([startDate, endDate]);
 };
