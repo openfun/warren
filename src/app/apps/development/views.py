519a065337570367d4af843a5fa017066a462167
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2024-06-20 19:04:09 +0200

    🔨(project) add development view for lti select page
    A development view for sending LTI requests already exists, but there was no
    development view for the LTI select page.
    Adding the missing view.

--- src/app/apps/development/views.py
+++ src/app/apps/development/views.py
@@ -18,13 +18,13 @@ logger = getLogger(__name__)
 
 @method_decorator(csrf_exempt, name="dispatch")
 @method_decorator(xframe_options_exempt, name="dispatch")
-class DevelopmentLTIView(TemplateView):
-    """A development view with iframe POST / plain POST helpers.
+class DevelopmentLTIRequestView(TemplateView):
+    """A development view for LTI requests with iframe POST / plain POST helpers.
 
     Not available outside of DEBUG = true environments.
     """
 
-    template_name = "development/lti_development.html"
+    template_name = "development/lti_request.html"
 
     def get_context_data(self, **kwargs):
         """Pre-populate fields in the LTI request form.
@@ -76,7 +76,12 @@ class DevelopmentLTIView(TemplateView):
         # use the HTTP_REFERER like to be consistent with the LTI passport
         launch_url = (
             urlparse(self.request.build_absolute_uri())
-            ._replace(path=reverse("lti:lti-request-view", kwargs={"selection": route}))
+            ._replace(
+                path=reverse(
+                    "lti:lti-request-view",
+                    kwargs={"selection": route},
+                )
+            )
             .geturl()
         )
         try:
@@ -127,3 +132,120 @@ class DevelopmentLTIView(TemplateView):
                 "launch_url": launch_url,
             }
         )
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(xframe_options_exempt, name="dispatch")
+class DevelopmentLTISelectView(TemplateView):
+    """A development view for LTI select with iframe POST / plain POST helpers.
+
+    Not available outside of DEBUG = true environments.
+    """
+
+    template_name = "development/lti_select.html"
+
+    def get_context_data(self, **kwargs):
+        """Pre-populate fields in the LTI select form.
+
+        Parameters
+        ----------
+        kwargs : dictionary
+            keyword extra arguments
+
+        Returns:
+        -------
+        dictionary
+            context for template rendering.
+        """
+        return {
+            "lti_message_type": "ContentItemSelectionRequest",
+            "lti_version": "LTI-1p0",
+            "lis_person_contact_email_primary": "admin@example.com",
+            "user_id": "1234",
+            "roles": "instructor",
+            "context_id": "2",
+            "context_title": "Mathematics 101",
+            "content_item_return_url": self.request.build_absolute_uri(
+                reverse("development:lti-select-view")
+            ),
+            "tool_consumer_instance_name": "FUN",
+            "tool_consumer_info_product_family_code": "moodle",
+            "accept_presentation_document_targets": "frame,iframe,window",
+            "accept_media_types": "application/vnd.ims.lti.v1.ltilink",
+        }
+
+    # pylint: disable=unused-argument
+    def post(self, request, *args, **kwargs):
+        """Respond to POST request on the refresh button.
+
+        Context populated with POST request.
+
+        Parameters
+        ----------
+        request : Request
+            passed by Django
+        args : list
+            positional extra arguments
+        kwargs : dictionary
+            keyword extra arguments
+
+        -------
+        HTML
+            generated from applying the data to the template
+
+        """
+        request_params = request.POST.dict()
+        excluded_params = ["refresh_signature"]
+        lti_parameters = {
+            key: value
+            for key, value in request_params.items()
+            if key not in excluded_params and "oauth" not in key
+        }
+
+        # use the HTTP_REFERER like to be consistent with the LTI passport
+        select_url = (
+            urlparse(self.request.build_absolute_uri())
+            ._replace(path=reverse("lti:lti-select-view"))
+            .geturl()
+        )
+        try:
+            lti_consumer = LTIConsumer.objects.get(url=select_url)
+        except LTIConsumer.DoesNotExist:
+            lti_consumer, _ = LTIConsumer.objects.get_or_create(
+                slug="localhost",
+                title="localhost test",
+                url=select_url,
+            )
+
+        passport, _ = LTIPassport.objects.get_or_create(
+            consumer=lti_consumer, title="Development passport"
+        )
+
+        oauth_client = oauth1.Client(
+            client_key=passport.oauth_consumer_key,
+            client_secret=passport.shared_secret,
+        )
+        # Compute Authorization header which looks like:
+        # Authorization: OAuth oauth_nonce="80966668944732164491378916897",
+        # oauth_timestamp="1378916897", oauth_version="1.0",
+        # oauth_signature_method="HMAC-SHA1", oauth_consumer_key="",
+        # oauth_signature="frVp4JuvT1mVXlxktiAUjQ7%2F1cw%3D"
+        _uri, headers, _body = oauth_client.sign(
+            select_url,
+            http_method="POST",
+            body=lti_parameters,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        # Parse headers to pass to template as part of context:
+        oauth_dict = dict(
+            param.strip().replace('"', "").split("=")
+            for param in headers["Authorization"].split(",")
+        )
+
+        signature = oauth_dict["oauth_signature"]
+        oauth_dict["oauth_signature"] = unquote(signature)
+        oauth_dict["oauth_nonce"] = oauth_dict.pop("OAuth oauth_nonce")
+        lti_parameters.update({"oauth_dict": oauth_dict})
+
+        return self.render_to_response(lti_parameters)
