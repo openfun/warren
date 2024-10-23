d41ebff3079aab662a9bbd06c5bc88db146154cf
Quitterie Lucas <quitterielucas@outlook.fr>
2024-10-23 17:07:29 +0200

    💄(packages) add document activity dashboards
    In addition to `document` plugin, a frontend package is available to display
    indicators on a dashboard.

---
+++ src/frontend/packages/document/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./Datagrid";
+export * from "./DocumentFilters";
+export * from "./Metrics";
+export * from "./Downloads";
---
+++ src/frontend/packages/document/src/pages/Student/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * A React component responsible for rendering a dashboard of document statistics
+ * for student.
+ *
+ *
+ * @returns {JSX.Element} The JSX for the document statistics overview dashboard.
+ */
+export default () => {
+  return (
+    <div className="c__overview">
+      No student view available for document indicators
+    </div>
+  );
+};
